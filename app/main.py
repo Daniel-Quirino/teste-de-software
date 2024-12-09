0636cf47322eb9f3493309176f155d4a0d06c360
Bernardo Reis <bernardo.reis.almeida01@gmail.com>
2024-12-09 11:11:24 -0300

    Implementação da infraestrutura da interface e da primeira ação

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from services.invoice import InvoiceService
+from services.product import ProductService
+from services.customer import CustomerService
 
-invoice_service = InvoiceService()
+product_service = ProductService()
+customer_service = CustomerService()
+invoice_service = InvoiceService(product_service, customer_service)
 
 print("=== Gerenciador de Faturas ===")
 print("1. Adicionar Cliente")
@@ -11,8 +15,12 @@ print("5. Criar Fatura")
 print("6. Listar Faturas")
 print("7. Sair")
 
-input = input("Escolha uma opção: ")
+action = input("Escolha uma opção: ")
 
-match input:
+match action:
     case "1":
-        
+        name = input("Nome do cliente: ")
+        email = input("E-mail do cliente: ")
+        customer = customer_service.add_customer(name, email)
+
+        print("Cliente adicionado:", str(customer))
