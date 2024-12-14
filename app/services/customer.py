2dad9a629fe1cc6b97835f2838c8c3a76bd013ba
Daniel Quirino <danielpires6@hotmail.com>
2024-12-14 08:16:42 -0300

    feat(customer | product): added more validations

--- app/services/customer.py
+++ app/services/customer.py
@@ -1,5 +1,5 @@
 import json
-
+import re
 
 class Customer:
     """
@@ -51,13 +51,25 @@ class CustomerService:
 
     def add_customer(self, name: str, email: str):
         """
-        Appens a new customer to the list of customers.
+        Appends a new customer to the list of customers.
 
-        Raises Exception() if parameters are invalid.
+        Raises specific exceptions if parameters are invalid or email is not in valid format.
         """
-        # Checks if customer parameters are valid.
-        if email == "" or name == "":
-            raise Exception("Nome e/ou e-mail inválido(s)!")
+
+        if name == "":
+            raise ValueError("O nome não pode ser vazio.")
+
+        if email == "":
+            raise ValueError("O e-mail não pode ser vazio.")
+
+        # Regex for validating an Email
+        email_regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+
+        if not re.match(email_regex, email):
+            raise ValueError("Formato de e-mail inválido. Por favor, insira um e-mail correto.")
+
+        if any(customer.email == email for customer in self._customers):
+            raise ValueError("Este e-mail já está cadastrado.")
 
         customer = Customer(self._id_counter, name, email)
         self._customers.append(customer)
@@ -77,6 +89,10 @@ class CustomerService:
 
         Raises Exception() if customer is not found.
         """
+
+        if customer_id is None:
+            raise ValueError("O ID do cliente não pode ser nulo.")
+
         for customer in self._customers:
             if customer.id == customer_id:
                 return customer
@@ -121,6 +137,7 @@ class CustomerService:
                 self._customers[i] = Customer(customer_id, name, email)
                 return self._customers[i]
         raise Exception("Cliente não encontrado!")
+
 
     def delete_customer(self, customer_id: int):
         """
