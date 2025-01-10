d02aa8cabc2d4a6b72ff0561d08efe6dc7b4e190
Daniel Quirino <danielpires6@hotmail.com>
2025-01-10 07:58:26 -0300

    Adicionado testes para menu (#8)

--- tests/test_services_menu.py
+++ tests/test_services_menu.py
@@ -1,11 +1,35 @@
 import pytest
 from unittest.mock import patch, MagicMock
 from app.services.menu import MenuService
-
+import os 
 
 @pytest.fixture
 def menu():
     return MenuService()
+
+def test_clear_screen(menu):
+    # Arrange.
+    with patch("os.system") as mock_system:
+
+        # Act.
+        menu.clear_screen()
+
+        # Assert.
+        mock_system.assert_called_once_with("cls" if os.name == "nt" else "clear")
+
+
+def test_execute_with_error_handling_failure(menu):
+    # Arrange.
+    mock_callback = MagicMock(side_effect=Exception("Falha na operação"))
+    error_message = "\nErro:"
+
+    with patch("builtins.print") as mock_print:
+        # Act.
+        menu.execute_with_error_handling(mock_callback, "\nOperação deveria ter sucesso:", error_message)
+
+        # Assert.
+        mock_callback.assert_called_once()
+        mock_print.assert_called_with(error_message, str(Exception("Falha na operação")))
 
 
 def test_show_header_client(menu):
